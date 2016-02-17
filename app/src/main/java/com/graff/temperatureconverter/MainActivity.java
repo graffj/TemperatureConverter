c50695993993aeab0167b935f0c7fb27f0702a66
Jonathan Graff <graffj99@yahoo.com>
2016-02-17 13:11:58 -0500

    Added degree label

--- app/src/main/java/com/graff/temperatureconverter/MainActivity.java
+++ app/src/main/java/com/graff/temperatureconverter/MainActivity.java
@@ -2,9 +2,11 @@ package com.graff.temperatureconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,11 +21,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick(View view) {
+
+        Log.d("MainActivity", "Entered onClick()");
+
         switch (view.getId()) {
 
             case R.id.button1:
                 RadioButton celsiusButton = (RadioButton) findViewById(R.id.radio0);
                 RadioButton fahrenheitButton = (RadioButton) findViewById(R.id.radio1);
+                TextView degreeLabel = (TextView) findViewById(R.id.degreeLabel);
 
                 /* Invalid numbers will have a length of 0; recognize and remind user to use numbers */
                 if (text.getText().length() == 0) {
@@ -37,13 +43,31 @@ public class MainActivity extends AppCompatActivity {
                     text.setText(String.valueOf(ConverterUtil.convertFahrenheitToCelsius(inputValue)));
                     celsiusButton.setChecked(false);
                     fahrenheitButton.setChecked(true);
+                    degreeLabel.setText(R.string.degreesC);
                 }
                 else {
                     text.setText(String.valueOf(ConverterUtil.convertCelsiusToFahrenheit(inputValue)));
                     fahrenheitButton.setChecked(false);
                     celsiusButton.setChecked(true);
+                    degreeLabel.setText(R.string.degreesF);
                 }
                 break;
+        }
+    }
+
+    public void onRadioClick(View view) {
+
+        Log.d("MainActivity", "Entered onRadioClick()");
+
+        RadioButton celsiusButton = (RadioButton) findViewById(R.id.radio0);
+        RadioButton fahrenheitButton = (RadioButton) findViewById(R.id.radio1);
+        TextView degreeLabel = (TextView) findViewById(R.id.degreeLabel);
+
+        if (celsiusButton.isChecked()) {
+            degreeLabel.setText(R.string.degreesF);
+        }
+        else {
+            degreeLabel.setText(R.string.degreesC);
         }
     }
 
